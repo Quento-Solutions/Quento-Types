e3cf2e53dcf7d0e4a1ebded46149a027cac8368d
Jeffrey Li <dr.cheddar@outlook.com>
2020-08-29 17:31:28 -0400

    Quento Types First Commit

---
+++ index.ts
@@ -0,0 +1,10 @@
+export * as Notes from './notes';
+export * as Subjects from './subjects';
+export * as Suggestions from './suggestions'
+export * as User from './user';
+export * as School from './schools';
+export * as Questions from './questions';
+export * as Responses from './responses';
+export * as Newsletter from './newsletters';
+
+export * as FirebaseTypes from './firebaseTypes';
---
+++ schools.ts
@@ -0,0 +1,12 @@
+export const SchoolList = [
+  'Colonel By Secondary School',
+  'Lisgar Collegiate Institute',
+  'Earl of March Secondary School',
+  'Bell High School',
+  'Longfields-Davidson Heights Secondary School',
+  'Merivale High School',
+  'John McCrae Secondary School',
+  'Other'
+] as const
+
+export type School_O = typeof SchoolList[number]
